84b0c8c8f9b60723bc6ca688dac9c2bedaa9fb63
kikimetal <reezafratzitz2@gmail.com>
2018-10-12 07:48:18 +0900

    ページ追加

---
+++ pages/event.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="page">
+    <div id="contact">
+      <h1>u-25</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () { this.$store.state.so() },
+  head () { return this.$store.state.getHead() },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
